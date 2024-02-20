7678df99880d1edba292806e8873f72a3233a14f
Paul Adamski <pauladamski1985@gmail.com>
2024-02-20 12:50:25 +0100

    update some bugs...

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-import express from 'express'
-import booksRouter from './routes/books.js'
-import recordsRouter from './routes/records.js'
-import log from './middleware/logMiddleware.js'
+import express from 'express';
+import booksRouter from './routes/books.js';
+import recordsRouter from './routes/records.js';
+import loginRouter from './routes/login.js';
+import log from './middleware/logMiddleware.js';
+import 'dotenv/config';
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use(log)
+app.use(log);
 
-app.use('/books', booksRouter)
-app.use('/records', recordsRouter)
+app.use('/books', booksRouter);
+app.use('/records', recordsRouter);
+app.use('/login', loginRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(3000, () => {
-  console.log(' \n beep.... \n .... \n Server is listening on port 3000 \n .... \n beep.... \n .... \n http://localhost:3000/books')
-})
+  console.log('Server is listening on port 3000');
+});
+
